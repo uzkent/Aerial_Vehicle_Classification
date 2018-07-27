12411537aaf0f19bb9a6d3259416c3e395bbd26b
buzkent86 <uzkent.burak@gmail.com>
2018-07-27 16:09:41 -0700

    wrong offset bug for chipping fixed

--- localization/prepare_dataset/prepare_xview.py
+++ localization/prepare_dataset/prepare_xview.py
@@ -20,8 +20,8 @@ def get_parser():
 def image_chipper(parent_img_name, chip_size, output_dir):
     """ Chips the full xview images and saves them into the output directory """
     img_arr = np.asarray(Image.open(parent_img_name))
-    for i in range(0, img_arr.shape[0]/chip_size):
-        for j in range(0, img_arr.shape[1]/chip_size):
+    for i in range(0, round(img_arr.shape[0]/chip_size)):
+        for j in range(0, round(img_arr.shape[1]/chip_size)):
             img_chip = img_arr[i*chip_size:i*chip_size+chip_size, j*chip_size:j*chip_size+chip_size, :]
             chip_name = '{}/{}_{}_{}.jpeg'.format(output_dir, os.path.splitext(parent_img_name.split('/')[-1])[0], i, j)
             img_chip = Image.fromarray(img_chip)
@@ -33,13 +33,14 @@ def ground_truth_parser(parent_img_name, chip_name, ground_truth, coords, chip_s
         in the chip of interest and saves them into a json file
     """
     chip_ground_truth = []
+    offset = coords[0] - chip_size
     for dict_ann in ground_truth['features']:
         if dict_ann['properties']['image_id'] == parent_img_name.split('/')[-1]:
             parent_img_coords = dict_ann['properties']['bounds_imcoords'].split(',')
-            if int(parent_img_coords[0]) > (coords[0] - chip_size) and int(parent_img_coords[1]) > (coords[1] - chip_size):
-                if int(parent_img_coords[2]) < (coords[0]) and int(parent_img_coords[3]) < (coords[1]):
-                    chip_ground_truth.append([[int(parent_img_coords[0])-coords[0], int(parent_img_coords[1])-coords[1],
-                    int(parent_img_coords[2])-coords[0], int(parent_img_coords[3])-coords[1]], dict_ann['properties']['type_id']])
+            if int(parent_img_coords[0]) > offset and int(parent_img_coords[1]) > offset:
+                if int(parent_img_coords[2]) < coords[0] and int(parent_img_coords[3]) < coords[1]:
+                    chip_ground_truth.append([[int(parent_img_coords[0]) - offset, int(parent_img_coords[1]) - offset,
+                    int(parent_img_coords[2]) - offset, int(parent_img_coords[3])-offset], dict_ann['properties']['type_id']])
 
     with open('{}{}'.format(os.path.splitext(chip_name)[0], '.json'), 'w') as output_file:
         json.dump(chip_ground_truth, output_file)
