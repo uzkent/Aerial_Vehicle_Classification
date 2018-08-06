7953a14b85e19ceba9f3b67d415966d4c550c17f
buzkent86 <uzkent.burak@gmail.com>
2018-08-06 16:23:55 -0700

    batch normalization added, code refactoring

--- resnet50_tensorflow/prepare_dataset.py
+++ resnet50_tensorflow/prepare_dataset.py
@@ -1,4 +1,15 @@
 import tensorflow as tf
+import pdb
+
+def get_data(file_names, file_labels, batch_size):
+    """ This function returns a pointer to iterate over the batches of data """
+    train_dataset = tf.data.Dataset.from_tensor_slices((file_names, file_labels))
+    train_dataset = train_dataset.map(parse_function)
+    train_dataset = train_dataset.repeat(2000)
+    train_batched_dataset = train_dataset.batch(batch_size)
+    train_iterator = train_batched_dataset.make_initializable_iterator()
+
+    return train_iterator.get_next(), train_iterator
 
 def parse_function(filename, label):
     """ Reads an image from a file, decodes it into a dense tensor, and resizes it
@@ -9,14 +20,14 @@ def parse_function(filename, label):
     image_scaled = tf.image.per_image_standardization(image_resized)
     return image_scaled, label
 
-def dataset_iterator(full_path, filenames):
+def dataset_iterator(full_path, filenames, arr):
     """ This function returns an iterator to the dataset """
     # Save all the file names and labels
     file_names = []
     file_labels = []
-    for file_information in filenames:
-        file_names.append('{}/{}'.format(full_path, file_information[0]))
-        file_labels.append(file_information[1])
+    for index in range(len(filenames)):
+        file_names.append('./{}/{}'.format(full_path, filenames[arr[index]][0].decode('UTF-8')))
+        file_labels.append(filenames[arr[index]][1])
 
     file_names = tf.constant(file_names)
     file_labels = tf.one_hot(file_labels, 2)
--- resnet50_tensorflow/resnet50_vehicle_classification.py
+++ resnet50_tensorflow/resnet50_vehicle_classification.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import numpy as np
 import argparse
+import prepare_dataset
+import pdb
 from tqdm import tqdm
-
-from prepare_dataset import dataset_iterator, parse_function
 
 def get_parser():
     """ This function returns a parser object """
@@ -49,12 +49,18 @@ class ResNet50():
         self.variable_summaries(bias)
         return bias
 
-    def _conv2d(self, input_data, shape, bias_shape, stride, filter_id, padding='SAME'):
+    def _batch_norm(self, input, filter_id, is_training):
+        """ Apply Batch Normalization After Convolution and Before Activation """
+        input_norm = tf.contrib.layers.batch_norm(input, center=True, scale=True, is_training=is_training)
+        return input_norm
+
+    def _conv2d(self, input_data, shape, bias_shape, stride, filter_id, is_training, padding='SAME'):
         """ Perform 2D convolution on the input data and apply RELU """
         weights = self.weight_variable(shape, 'weights' + filter_id)
         bias = self.bias_variable(bias_shape, 'bias' + filter_id)
         output_conv = tf.nn.conv2d(input_data, weights, strides=stride, padding='SAME')
-        return tf.nn.relu(output_conv + bias)
+        output_conv_norm = self._batch_norm(output_conv + bias, filter_id, is_training)
+        return tf.nn.relu(output_conv_norm + bias)
 
     def _fcl(self, input_data, shape, bias_shape, filter_id, classification_layer=False):
         """ Run a Fully Connected Layer and ReLU if necessary """
@@ -68,74 +74,62 @@ class ResNet50():
             return tf.nn.relu(tf.matmul(out_fc_layer, weights) + bias)
 
     def resnet50_block(self, input_feature_map, number_bottleneck_channels,
-    number_input_channels, number_output_channels, stride=[1, 1, 1, 1]):
+    number_input_channels, number_output_channels, is_training, stride=[1, 1, 1, 1]):
         """ Run a ResNet block """
         out_1 = self._conv2d(input_feature_map, [1, 1, number_input_channels, number_bottleneck_channels],
-        [number_bottleneck_channels], [1, 1, 1, 1], 'bottleneck_down')
+        [number_bottleneck_channels], [1, 1, 1, 1], 'bottleneck_down', is_training)
         out_2 = self._conv2d(out_1, [3, 3, number_bottleneck_channels, number_bottleneck_channels],
-        [number_bottleneck_channels], stride, 'conv3x3')
+        [number_bottleneck_channels], stride, 'conv3x3', is_training)
         out_3 = self._conv2d(out_2, [1, 1, number_bottleneck_channels, number_output_channels],
-        [number_output_channels], [1, 1, 1, 1], 'bottleneck_up')
+        [number_output_channels], [1, 1, 1, 1], 'bottleneck_up', is_training)
         identity_mapping = self._conv2d(input_feature_map, [1, 1, number_input_channels, number_output_channels],
-        [number_output_channels], stride, 'identity_mapping')
+        [number_output_channels], stride, 'identity_mapping', is_training)
         return tf.add(identity_mapping, out_3)
 
     def resnet50_module(self, input_data, number_blocks, number_bottleneck_channels, number_input_channels,
-    number_output_channels, stride=[1, 2, 2, 1]):
+                    number_output_channels, is_training, stride=[1, 2, 2, 1]):
         """ Run a ResNet module consisting of residual blocks """
         for index, block in enumerate(range(number_blocks)):
             if index == 0:
                 with tf.variable_scope('module' + str(index)):
                     out = self.resnet50_block(input_data, number_bottleneck_channels, number_input_channels,
-                    number_output_channels, stride=stride)
+                    number_output_channels, is_training, stride=stride)
             else:
                 with tf.variable_scope('module' + str(index)):
                     out = self.resnet50_block(out, number_bottleneck_channels, number_output_channels,
-                    number_output_channels, stride=[1, 1, 1, 1])
+                    number_output_channels, is_training, stride=[1, 1, 1, 1])
 
         return out
 
-def get_data(file_names, file_labels, batch_size):
-    """ This function returns a pointer to iterate over the batches of data """
-    train_dataset = tf.data.Dataset.from_tensor_slices((file_names, file_labels))
-    train_dataset = train_dataset.map(parse_function)
-    train_dataset = train_dataset.repeat(200)
-    train_dataset = train_dataset.shuffle(buffer_size=1000)
-    train_batched_dataset = train_dataset.batch(batch_size)
-    train_iterator = train_batched_dataset.make_initializable_iterator()
-
-    return train_iterator.get_next(), train_iterator
-
 def main():
+
     args = get_parser().parse_args()
 
     # Read the filenames in the DIRSIG Vehicle Classification Dataset
     train_filenames = np.genfromtxt('{}/{}'.format(args.train_dir, 'train.txt'), delimiter=' ', dtype=None)
+    arr = np.arange(len(train_filenames))
+    np.random.shuffle(arr)
 
     # Prepare the training dataset
-    file_names, file_labels = dataset_iterator(args.train_dir, train_filenames)
-    train_batch, train_iterator = get_data(file_names, file_labels, args.batch_size)
+    file_names, file_labels = prepare_dataset.dataset_iterator(args.train_dir, train_filenames, arr)
+    train_batch, train_iterator = prepare_dataset.get_data(file_names, file_labels, args.batch_size)
     x_train = tf.placeholder(tf.float32, [args.batch_size, 56, 56, 3])
     y_train = tf.placeholder(tf.int32, [None, 2])
+    is_training = tf.placeholder(tf.bool)
     tf.summary.image("training_input_image", x_train, max_outputs=20)
 
     # Build the Graph
     net = ResNet50()
     with tf.variable_scope("FirstStageFeatureExtractor") as scope:
-        out_1 = net._conv2d(x_train, [3, 3, 3, 64], [64], [1, 1, 1, 1], 'conv3x3')
-
+        out_1 = net._conv2d(x_train, [3, 3, 3, 64], [64], [1, 1, 1, 1], 'conv3x3', is_training)
     with tf.variable_scope("ResNetBlock1"):
-        out_2 = net.resnet50_module(out_1, 3, 64, 64, 256, [1, 1, 1, 1])
-
+        out_2 = net.resnet50_module(out_1, 3, 64, 64, 256, is_training, [1, 1, 1, 1])
     with tf.variable_scope("ResNetBlock2"):
-        out_3 = net.resnet50_module(out_2, 4, 128, 256, 512)
-
+        out_3 = net.resnet50_module(out_2, 4, 128, 256, 512, is_training)
     with tf.variable_scope("ResNetBlock3"):
-        out_4 = net.resnet50_module(out_3, 6, 256, 512, 1024)
-
+        out_4 = net.resnet50_module(out_3, 6, 256, 512, 1024, is_training)
     with tf.variable_scope("ResNetBlock4"):
-        out_5 = net.resnet50_module(out_4, 3, 512, 1024, 2048)
-
+        out_5 = net.resnet50_module(out_4, 3, 512, 1024, 2048, is_training)
     with tf.variable_scope("PredictionBlock"):
         out_6 = tf.nn.pool(out_5, window_shape=[7, 7], pooling_type='AVG', padding="VALID")
         out_7 = net._fcl(out_6, [2048, 1024], [1024], 'fc_1')
@@ -144,7 +138,10 @@ def main():
     # Define the loss function and optimizer
     cross_entropy = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(labels=y_train, logits=y_pred))
-    optimizer = tf.train.GradientDescentOptimizer(args.learning_rate).minimize(cross_entropy)
+    optimizer = tf.train.AdamOptimizer(args.learning_rate)
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+         train_op = optimizer.minimize(cross_entropy)
     tf.summary.scalar('cross_entropy', cross_entropy)
 
     # Define the Classification Accuracy
@@ -163,14 +160,14 @@ def main():
         sess.run(tf.global_variables_initializer())
         sess.run(train_iterator.initializer)
         for epoch_number in tqdm(range(args.number_epochs)):
-            summary, _, loss_value = sess.run([merged, optimizer, cross_entropy], feed_dict={x_train: train_batch[0].eval(session=sess),
+            summary, _, loss_value = sess.run([merged, train_op, cross_entropy], feed_dict={is_training : True, x_train: train_batch[0].eval(session=sess),
             y_train: train_batch[1].eval(session=sess)})
             train_writer.add_summary(summary, epoch_number)
             print("Loss at iteration {} : {}".format(epoch_number, loss_value))
 
             # Run the model on the test data for validation
             if epoch_number % args.test_frequency == 0:
-                summary, acc = sess.run([merged, accuracy], feed_dict={
+                summary, acc = sess.run([merged, accuracy], feed_dict={is_training : False,
                 x_train:train_batch[0].eval(session=sess), y_train: train_batch[1].eval(session=sess)})
                 print("Accuracy at iteration {} : {}".format(epoch_number, acc))
                 test_writer.add_summary(summary, epoch_number)
