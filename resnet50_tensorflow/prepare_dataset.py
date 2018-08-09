cb64f8a2426ac128fdf561692bad73ad965b3754
buzkent86 <uzkent.burak@gmail.com>
2018-08-08 23:27:05 -0700

    First draft of updated regression implementation

--- resnet50_tensorflow/prepare_dataset.py
+++ resnet50_tensorflow/prepare_dataset.py
@@ -1,5 +1,29 @@
 import tensorflow as tf
+import os
 import pdb
+
+def top_k_label_finder(original_label, similarity_df):
+    """ This function find the top k labels that is mutually-inclusive to the label of interest """
+    search_df = similarity_df[similarity_df['anchor'] == original_label]
+    search_df['score_ranked'] = search_df['score'].rank(ascending=0)
+    inclusive_labels_original = []
+    for ind, row in search_df.iterrows():
+        if row['score_ranked'] == 2 or row['score_ranked'] == 3 or row['score_ranked'] == 4:
+           inclusive_labels_original.append(row['query'])
+    return inclusive_labels_original
+   
+def label_mapper(labels_df, similarity_df, original_label):
+    """ Maps the semantic label to a one-hot encoded labels """
+    label_index = labels_df[labels_df['Tag'] == original_label].index[0] + 1
+    inclusive_labels_original = top_k_label_finder(original_label, similarity_df)
+    inclusive_labels_ind = []
+    for inc_label in inclusive_labels_original:
+        try:
+            inclusive_labels_ind.append(labels_df[labels_df['Tag'] == inc_label].index[0] + 1)              
+        except Exception as error:
+            pdb.set_trace()
+            print('Label Mapping Error', error)
+    return label_index, inclusive_labels_ind
 
 def get_data(file_names, file_labels, batch_size):
     """ This function returns a pointer to iterate over the batches of data """
@@ -8,27 +32,30 @@ def get_data(file_names, file_labels, batch_size):
     train_dataset = train_dataset.repeat(2000)
     train_batched_dataset = train_dataset.batch(batch_size)
     train_iterator = train_batched_dataset.make_initializable_iterator()
-
     return train_iterator.get_next(), train_iterator
 
 def parse_function(filename, label):
     """ Reads an image from a file, decodes it into a dense tensor, and resizes it
     to a fixed shape """
     image_string = tf.read_file(filename)
-    image_decoded = tf.image.decode_jpeg(image_string)
-    image_resized = tf.image.resize_images(image_decoded, [56, 56])
+    image_decoded = tf.image.decode_jpeg(image_string, channels=3)
+    image_resized = tf.image.resize_images(image_decoded, [224, 224])
     image_scaled = tf.image.per_image_standardization(image_resized)
     return image_scaled, label
 
-def dataset_iterator(full_path, filenames, arr):
+def dataset_iterator(full_path, filenames, arr, labels_df, similarity_df, threshold=500):
     """ This function returns an iterator to the dataset """
     file_names = []
     file_labels = []
     for index in range(len(filenames)):
-        file_names.append('./{}/{}'.format(full_path, filenames[arr[index]][0].decode('UTF-8')))
-        file_labels.append(filenames[arr[index]][1])
+        if not os.path.isfile('{}/{}{}.{}'.format(full_path, 'dg_wiki_uganda_1000x1000_', filenames['id'][arr[index]], 'jpeg')):
+            continue
+        file_names.append('{}/{}{}.{}'.format(full_path, 'dg_wiki_uganda_1000x1000_', filenames['id'][arr[index]], 'jpeg'))
+        file_labels.append(label_mapper(labels_df, similarity_df, filenames['category'][arr[index]]))
 
     file_names = tf.constant(file_names)
-    file_labels = tf.one_hot(file_labels, 2)
-
+    pdb.set_trace()
+    np_labels = np.zeros((len(labels_df), np.int32))
+    np_labels[np.array(inclusive_labels_ind)] = 1
+    file_labels = tf.convert_to_tensor(np_labels)
     return file_names, file_labels
--- resnet50_tensorflow/resnet50_vehicle_classification.py
+++ resnet50_tensorflow/resnet50_vehicle_classification.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import argparse
 import prepare_dataset
+import pandas as pd
 import pdb
 from tqdm import tqdm
 
@@ -103,47 +104,55 @@ class ResNet50():
 
 def main():
 
+    # Parse the command line args
     args = get_parser().parse_args()
 
     # Read the filenames in the DIRSIG Vehicle Classification Dataset
-    train_filenames = np.genfromtxt('{}/{}'.format(args.train_dir, 'train.txt'), delimiter=' ', dtype=None)
-    val_filenames = np.genfromtxt('{}/{}'.format(args.test_dir, 'val.txt'), delimiter=' ', dtype=None)
+    similarity_df = pd.read_csv('./curated_category_similarities.csv')
+    train_filenames = pd.read_csv('/atlas/u/chenlin/wikipedia_images/Uganda_filtered_category_coordinate.csv')
+    val_filenames = train_filenames
+    labels_df = pd.read_csv('category_bounding_box.csv')
     arr = np.arange(len(train_filenames))
     np.random.shuffle(arr)
 
     # Prepare the training dataset
-    file_names, file_labels = prepare_dataset.dataset_iterator(args.train_dir, train_filenames, arr)
+    file_names, file_labels = prepare_dataset.dataset_iterator(args.train_dir, train_filenames, arr, labels_df, similarity_df, 5000)
     train_batch, train_iterator = prepare_dataset.get_data(file_names, file_labels, args.batch_size)
-    x_train = tf.placeholder(tf.float32, [None, 56, 56, 3])
-    y_train = tf.placeholder(tf.int32, [None, 2])
+    x_train = tf.placeholder(tf.float32, [None, 224, 224, 3])
+    y_train = tf.placeholder(tf.int32, [None, len(labels_df)])
     is_training = tf.placeholder(tf.bool)
     tf.summary.image("training_input_image", x_train, max_outputs=20)
 
     # Prepare the validation dataset
     arr = np.arange(len(val_filenames))
-    file_names, file_labels = prepare_dataset.dataset_iterator(args.test_dir, val_filenames, arr)
+    np.random.shuffle(arr)
+    file_names, file_labels = prepare_dataset.dataset_iterator(args.test_dir, val_filenames, arr, labels_df, similarity_df, 50)
     val_batch, val_iterator = prepare_dataset.get_data(file_names, file_labels, len(arr))
 
     # Build the Graph
     net = ResNet50()
     with tf.variable_scope("FirstStageFeatureExtractor") as scope:
-        out_1 = net._conv2d(x_train, [3, 3, 3, 64], [64], [1, 1, 1, 1], 'conv3x3', is_training)
+        out_1 = net._conv2d(x_train, [7, 7, 3, 64], [64], [1, 2, 2, 1], 'conv3x3', is_training)
+        out_1_max = tf.nn.max_pool(out_1, [1, 2, 2, 1], [1, 2, 2, 1], padding='SAME', name='max_pool')
     with tf.variable_scope("ResNetBlock1"):
-        out_2 = net.resnet50_module(out_1, 3, 64, 64, 256, is_training, [1, 1, 1, 1])
+        out_2 = net.resnet50_module(out_1_max, 3, 64, 64, 256, is_training, [1, 1, 1, 1])
+    for index in range(20):
+        tf.summary.image("ResNet_Block1_Feature_Maps", tf.expand_dims(out_2[:, :, :, index], -1),max_outputs=20)
     with tf.variable_scope("ResNetBlock2"):
         out_3 = net.resnet50_module(out_2, 4, 128, 256, 512, is_training)
     with tf.variable_scope("ResNetBlock3"):
         out_4 = net.resnet50_module(out_3, 6, 256, 512, 1024, is_training)
+    for index in range(20):
+        tf.summary.image("ResNet_Block3_Feature_Maps", tf.expand_dims(out_4[:, :, :, index], -1), max_outputs=20)
     with tf.variable_scope("ResNetBlock4"):
         out_5 = net.resnet50_module(out_4, 3, 512, 1024, 2048, is_training)
     with tf.variable_scope("PredictionBlock"):
         out_6 = tf.nn.pool(out_5, window_shape=[7, 7], pooling_type='AVG', padding="VALID")
         out_7 = net._fcl(out_6, [2048, 1024], [1024], 'fc_1')
-        y_pred = net._fcl(out_7, [1024, 2], [2], 'fc_2', classification_layer=True)
+        y_pred = net._fcl(out_7, [1024, len(labels_df)], [len(labels_df)], 'fc_2', classification_layer=True)
 
     # Define the loss function and optimizer
-    cross_entropy = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(labels=y_train, logits=y_pred))
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_train, logits=y_pred))
     optimizer = tf.train.AdamOptimizer(args.learning_rate)
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     with tf.control_dependencies(update_ops):
@@ -155,6 +164,7 @@ def main():
         correct_prediction = tf.equal(tf.argmax(y_pred,1), tf.argmax(y_train,1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     tf.summary.scalar('accuracy', accuracy)
+    tf.summary.text('Labels', tf.as_string(y_train))
 
     # Merge all visualized parameters
     merged = tf.summary.merge_all()
@@ -171,13 +181,13 @@ def main():
             y_train: train_batch[1].eval(session=sess)})
             train_writer.add_summary(summary, epoch_number)
             print("Loss at iteration {} : {}".format(epoch_number, loss_value))
-
+            
             # Run the model on the test data for validation
             if epoch_number % args.test_frequency == 0:
                 summary, acc = sess.run([merged, accuracy], feed_dict={is_training : False,
                 x_train: val_batch[0].eval(session=sess), y_train: val_batch[1].eval(session=sess)})
                 print("Accuracy at iteration {} : {}".format(epoch_number, acc))
                 test_writer.add_summary(summary, epoch_number)
-
+            
 if __name__ == '__main__':
     main()
